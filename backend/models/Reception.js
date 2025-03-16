0d31ca00c76007334a749dc2a0ffc0d7a823540b
Athul-k-ak <athulk8888@gmail.com>
2025-03-16 19:08:20 +0530

    version 1.02

--- backend/models/Reception.js
+++ backend/models/Reception.js
@@ -4,6 +4,7 @@ const receptionSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  phone: { type: String, required: true },
   role: { type: String, default: "reception" }, // Ensure role is set
   profileImage: { type: String },
 });
